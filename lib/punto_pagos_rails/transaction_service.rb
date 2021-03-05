54c45518c7014349cefdca330815bcab864c8d39
juannodeb <juannodeb@gmail.com>
2021-03-05 11:08:24 -0500

    test: more puts

--- lib/punto_pagos_rails/transaction_service.rb
+++ lib/punto_pagos_rails/transaction_service.rb
@@ -18,6 +18,8 @@ module PuntoPagosRails
       if !response.success?
         puts "PPRAILS RESPONSE =====> #{response.inspect}"
         puts "PPRAILS ERRORS =====> #{response.errors}"
+        puts "PPRAILS PAYABLE =====> #{payable.inspect}"
+        puts "PPRAILS PAYABLE ERRORS =====> #{payable.errors}"
         payable.errors.add(:base, I18n.t("punto_pagos_rails.errors.invalid_puntopagos_response"))
         return false
       end
